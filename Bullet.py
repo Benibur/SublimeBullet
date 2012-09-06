5c546af027a18275972c307c7c8ede4f9742fd80
Chris Barnett <barney.chrisj@gmail.com>
2012-09-06 22:33:59 +1000

    refactored Bullet.py to improve code reuse

--- Bullet.py
+++ Bullet.py
@@ -6,12 +6,11 @@ import re
 # TODO: Better multi line pasting behaviour (esp the bug that if you paste a 2 lines content, a * will be added to the end of the pasting )
 #       Just check whether the line is a empty line before inserting
 # TODO: Can try to write to keymap files during start of sublime text. So the when one change the settings it will change the keymapping
-# TODO: Write a text file in the plugin to indicate whether the settings are updated or not. When updated, write the keymap file accordingly. 
+# TODO: Write a text file in the plugin to indicate whether the settings are updated or not. When updated, write the keymap file accordingly.
 # TODO: Also, put instruction text in the keymap file say they shouldn't manage the keymap file directly. If they really want to, they should put a false in the settings file "manage keymap autoamtically" part first.
 
 class Bullet(sublime_plugin.EventListener):
   Modifying = False
-  #is_markdown = False
   md_enabled = False
   rest_enabled = False
   file_type = 0
@@ -19,122 +18,82 @@ class Bullet(sublime_plugin.EventListener):
   last_pos = 0
   #selectors = []
   selector_array = []
+  bullet_chars = [
+    "",
+    "* - > +", # Markdown
+    u"* - + \u2022 \u2043 \u2023" # reStructuredText (* - + • ⁃ ‣)
+  ]
 
   def __init__(self):
     s = sublime.load_settings("Bullet.sublime-settings")
-    Bullet.md_enabled = s.get("markdown_bullet_enabled", True)
-    Bullet.rest_enabled = s.get("restructuredtext_bullet_enabled", False)
-    #Bullet.selector_array = selectors.split("|")
-    #Bullet.selector_array = ["text.html.markdown"]
+    self.md_enabled = s.get("markdown_bullet_enabled", True)
+    self.rest_enabled = s.get("restructuredtext_bullet_enabled", False)
+    #self.selector_array = selectors.split("|")
+    #self.selector_array = ["text.html.markdown"]
 
   def on_activated(self, view):
     md_score = 0
     rest_score = 0
-    if Bullet.md_enabled:
+    if self.md_enabled:
       md_score = view.score_selector(0,'text.html.markdown')
-    if Bullet.rest_enabled:
+    if self.rest_enabled:
       rest_score = view.score_selector(0,'text.restructuredtext')
     if md_score > 0 and md_score >= rest_score:
-      Bullet.file_type = 1
+      self.file_type = 1
     elif rest_score > 0 and rest_score >= md_score:
-      Bullet.file_type = 2
+      self.file_type = 2
     else:
-      Bullet.file_type = 0
+      self.file_type = 0
 
-    #for x in range(len(Bullet.selector_array)):
-    #  if (view.score_selector(0,Bullet.selector_array[x]) > 0):
-    #    Bullet.is_markdown = True
-    #    Bullet.update_row(self, view)
+    #for x in range(len(self.selector_array)):
+    #  if (view.score_selector(0,self.selector_array[x]) > 0):
+    #    self.is_markdown = True
+    #    self.update_row(view)
     #    return
     #  else:
-    #    Bullet.is_markdown = False
-  
+    #    self.is_markdown = False
+
   def on_selection_modified(self, view):
-    #if (Bullet.is_markdown):
-    if (Bullet.file_type > 0):
-      Bullet.update_row(self, view)
-    
+    if (self.file_type > 0):
+      self.update_row(view)
+
   def update_row(self, view):
     loc = view.sel()[0]
-    Bullet.last_pos = loc.begin()
-    row, col = view.rowcol(Bullet.last_pos)
-    Bullet.last_line = row
+    self.last_pos = loc.begin()
+    row, col = view.rowcol(self.last_pos)
+    self.last_line = row
 
   def on_modified(self, view):
-    if Bullet.Modifying == False:
-      Bullet.Modifying = True
-      if Bullet.file_type == 1:
-        loc = view.sel()[0]
-        row, col = view.rowcol(loc.begin())
-        point_last_row = view.text_point(row - 1,0)
-        if (row - Bullet.last_line == 1):
-          previous_line = view.substr(view.line(Bullet.last_pos))
-          if row != 0 and previous_line != "":
-            match_pattern = re.search("^( *|\t*)(\*|\-|\>|\+|[0-9]+(\.))(.*)",previous_line)
-            if match_pattern != None:
-              if match_pattern.group(4) == " " or match_pattern.group(4) == "":
-                reg_remove = view.find("(\*|\-|\>|\+|[0-9]+\.)(.*)",point_last_row-1)
-                edit = view.begin_edit()
-                view.erase(edit,reg_remove)
-                view.end_edit(edit)
+    if self.Modifying == False:
+      self.Modifying = True
+      loc = view.sel()[0]
+      row, col = view.rowcol(loc.begin())
+      point_last_row = view.text_point(row - 1,0)
+      if (row - self.last_line == 1):
+        previous_line = view.substr(view.line(self.last_pos))
+        if row != 0 and previous_line != "":
+          bullet_chars = self.bullet_chars[self.file_type].split()
+          bullet_re = "|".join(map(re.escape, bullet_chars))
+          search_re = "^( *|\t*)(%s|([0-9]+)\.)(.*)" % bullet_re
+          match_pattern = re.search(search_re, previous_line)
+          if match_pattern != None:
+            if match_pattern.group(4) in [" ",""]:
+              # remove empty bullet point upon newline
+              reg_remove = view.find("\S.*", point_last_row)
+              edit = view.begin_edit()
+              view.erase(edit,reg_remove)
+              view.end_edit(edit)
+            else:
+              if match_pattern.group(3) != None:
+                # insert incremented number
+                last_number = int(match_pattern.group(3))
+                insertion = str(last_number+1) + ". "
               else:
-                if match_pattern.group(2) == "*":
-                  insertion = "* "
-                elif match_pattern.group(2) == "-":
-                  insertion = "- "
-                elif match_pattern.group(2) == ">":
-                  insertion = "> "
-                elif match_pattern.group(2) == "+":
-                  insertion = "+ "
-                else:
-                  match_number = re.search("[0-9]+",match_pattern.group(2))
-                  if match_number != None:
-                    last_number = int(match_number.group(0))
-                    insertion = str(last_number+1) + match_pattern.group(3) + " "
-                  else:
-                    insertion = ""
-                edit = view.begin_edit()
-                view.insert(edit, loc.end(), insertion)
-                view.end_edit(edit)
-      elif Bullet.file_type == 2:
-        loc = view.sel()[0]
-        row, col = view.rowcol(loc.begin())
-        point_last_row = view.text_point(row - 1,0)
-        if (row - Bullet.last_line == 1):
-          previous_line = view.substr(view.line(Bullet.last_pos))
-          if row != 0 and previous_line != "":
-            match_pattern = re.search(u"^( *|\t*)(\*|\-|\+|•|⁃|‣|#.|[0-9]+(\.))(.*)",previous_line)
-            if match_pattern != None:
-              if match_pattern.group(4) == " " or match_pattern.group(4) == "":
-                reg_remove = view.find(u"(\*|\-|\+|•|⁃|‣|#.|[0-9]+\.)(.*)",point_last_row-1)
-                edit = view.begin_edit()
-                view.erase(edit,reg_remove)
-                view.end_edit(edit)
-              else:
-                if match_pattern.group(2) == "*":
-                  insertion = "* "
-                elif match_pattern.group(2) == "-":
-                  insertion = "- "
-                elif match_pattern.group(2) == "+":
-                  insertion = "+ "
-                elif match_pattern.group(2) == u"•":
-                  insertion = u"• "
-                elif match_pattern.group(2) == u"⁃":
-                  insertion = u"⁃ "
-                elif match_pattern.group(2) == u"‣":
-                  insertion = u"‣ "
-                elif match_pattern.group(2) == "#.":
-                  insertion = "#. "
-                else:
-                  match_number = re.search("[0-9]+",match_pattern.group(2))
-                  if match_number != None:
-                    last_number = int(match_number.group(0))
-                    insertion = str(last_number+1) + match_pattern.group(3) + " "
-                  else:
-                    insertion = ""
-                edit = view.begin_edit()
-                view.insert(edit, loc.end(), insertion)
-                view.end_edit(edit)
-      
-    Bullet.update_row(self, view)
-    Bullet.Modifying = False
+                # insert bullet
+                insertion = match_pattern.group(2) + " "
+              edit = view.begin_edit()
+              view.insert(edit, loc.end(), insertion)
+              view.end_edit(edit)
+      self.update_row(view)
+      self.Modifying = False
+
