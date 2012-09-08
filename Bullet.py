f5f6ea4002a209abfeadbff2edfcf3c3aa6554a0
Chris Barnett <barney.chrisj@gmail.com>
2012-09-08 14:48:11 +1000

    use hidden selection to record last position fixed bug that prevented undo after Add Line Before macro used

--- Bullet.py
+++ Bullet.py
@@ -9,14 +9,16 @@ import re
 # TODO: Write a text file in the plugin to indicate whether the settings are updated or not. When updated, write the keymap file accordingly.
 # TODO: Also, put instruction text in the keymap file say they shouldn't manage the keymap file directly. If they really want to, they should put a false in the settings file "manage keymap autoamtically" part first.
 
+BULLET_SCOPE = "sublime_bullet"
+BULLET_LAST_POINT = "sublime_bullet_last_point"
+
 class Bullet(sublime_plugin.EventListener):
   Modifying = False
   md_enabled = False
   rest_enabled = False
   file_type = 0
-  last_row = 0
-  last_pos = 0
-  #selectors = []
+  # last_row = 0
+  # last_pos = 0
   selector_array = []
   bullet_chars = [
     "",
@@ -28,10 +30,9 @@ class Bullet(sublime_plugin.EventListener):
     s = sublime.load_settings("Bullet.sublime-settings")
     self.md_enabled = s.get("markdown_bullet_enabled", True)
     self.rest_enabled = s.get("restructuredtext_bullet_enabled", False)
-    #self.selector_array = selectors.split("|")
-    #self.selector_array = ["text.html.markdown"]
 
   def on_activated(self, view):
+    # determine file_type
     md_score = 0
     rest_score = 0
     if self.md_enabled:
@@ -45,40 +46,46 @@ class Bullet(sublime_plugin.EventListener):
     else:
       self.file_type = 0
 
-    #for x in range(len(self.selector_array)):
-    #  if (view.score_selector(0,self.selector_array[x]) > 0):
-    #    self.is_markdown = True
-    #    self.update_row(view)
-    #    return
-    #  else:
-    #    self.is_markdown = False
+    # update last pos
+    if self.file_type > 0:
+      self.update_last_pos(view)
+    else:
+      view.erase_regions(BULLET_LAST_POINT)
 
   def on_selection_modified(self, view):
-    if (self.file_type > 0):
-      self.update_row(view)
+    if self.file_type > 0 and self.Modifying == False:
+      self.update_last_pos(view)
 
-  def update_row(self, view):
-    loc = view.sel()[0]
-    self.last_pos = loc.begin()
-    row, col = view.rowcol(self.last_pos)
-    self.last_row = row
+  def update_last_pos(self, view):
+    last_line = view.line(view.sel()[0])
+    last_line_start = sublime.Region(last_line.begin())
+    view.add_regions(BULLET_LAST_POINT, [last_line_start], BULLET_SCOPE, "", sublime.HIDDEN)
+
+  def last_pos(self, view):
+    last_regions = view.get_regions(BULLET_LAST_POINT)
+    if len(last_regions) > 0:
+      return last_regions[0].begin()
+
+  def last_row(self, view):
+    last_pos = self.last_pos(view)
+    if last_pos is not None:
+      return view.rowcol(last_pos)[0]
 
   def on_modified(self, view):
-    if self.Modifying == False:
+    if self.file_type > 0 and self.Modifying == False:
       self.Modifying = True
       insert_point = view.sel()[0].begin()
       row, col = view.rowcol(insert_point)
       last_cmd, last_cmd_args, last_cmd_count = view.command_history(0)
+      non_undo = view.command_history(1)[0] == None
 
       # new line
-      if row != 0 and row - self.last_row == 1:
-        ref_row = row - 1 # row above
-        self.bullet_event(view, insert_point, ref_row)
-      elif last_cmd == "run_macro_file" and last_cmd_count == 1 \
-          and last_cmd_args == {'file': u'Packages/Default/Add Line Before.sublime-macro'}:
-        ref_row = row + 1
-        self.bullet_event(view, insert_point, ref_row)
-      self.update_row(view)
+      last_row = self.last_row(view)
+      if last_row is not None and row > 0 and abs(row - last_row) == 1:
+        self.bullet_event(view, insert_point, last_row)
+      # elif non_undo and last_cmd == "join_lines":
+        # TODO: remove leading bullet from joined lines
+      self.update_last_pos(view)
       self.Modifying = False
 
   def bullet_event(self, view, insert_point, ref_row):
@@ -88,7 +95,7 @@ class Bullet(sublime_plugin.EventListener):
     if ref_line_txt != "":
       bullet_chars = self.bullet_chars[self.file_type].split()
       bullet_re = "|".join(map(re.escape, bullet_chars))
-      search_re = "^( *|\t*)(%s|([0-9]+)\.)(.*)" % bullet_re
+      search_re = "^( *|\t*)(%s|([0-9]+)\.) (.*)" % bullet_re
       match_pattern = re.search(search_re, ref_line_txt)
       if match_pattern != None:
         if match_pattern.group(4) in [" ",""]:
@@ -106,5 +113,12 @@ class Bullet(sublime_plugin.EventListener):
             # insert bullet
             insertion = match_pattern.group(2) + " "
           edit = view.begin_edit()
-          view.insert(edit, insert_point, insertion)
+          # duplicate ref_line init whitespace
+          line_start = view.line(insert_point).begin()
+          init_whitespace = sublime.Region(line_start, insert_point)
+          replacement = match_pattern.group(1)+insertion
+          view.replace(edit, init_whitespace, replacement)
+          final_pos = view.sel()[0].end()
+          view.sel().clear()
+          view.sel().add(sublime.Region(final_pos))
           view.end_edit(edit)
