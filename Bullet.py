405e986d9ffded8a5348dae0388ecdeae906b349
Chris Barnett <barney.chrisj@gmail.com>
2012-09-08 17:53:22 +1000

    remove extra bullets on join_lines command refactoring

--- Bullet.py
+++ Bullet.py
@@ -17,9 +17,6 @@ class Bullet(sublime_plugin.EventListener):
   md_enabled = False
   rest_enabled = False
   file_type = 0
-  # last_row = 0
-  # last_pos = 0
-  selector_array = []
   bullet_chars = [
     "",
     "* - > +", # Markdown
@@ -53,13 +50,25 @@ class Bullet(sublime_plugin.EventListener):
       view.erase_regions(BULLET_LAST_POINT)
 
   def on_selection_modified(self, view):
-    if self.file_type > 0 and self.Modifying == False:
+    if self.file_type > 0:
       self.update_last_pos(view)
+
+  def match_bullet_line(self, text):
+    bullet_chars = self.bullet_chars[self.file_type].split()
+    bullet_re = "|".join(map(re.escape, bullet_chars))
+    search_re = "^( *|\t*)(%s|([0-9]+)\.)(.*)" % bullet_re
+    match_result = re.search(search_re, text)
+    return match_result
 
   def update_last_pos(self, view):
     last_line = view.line(view.sel()[0])
-    last_line_start = sublime.Region(last_line.begin())
-    view.add_regions(BULLET_LAST_POINT, [last_line_start], BULLET_SCOPE, "", sublime.HIDDEN)
+    last_line_txt = view.substr(last_line)
+    match_result = self.match_bullet_line(last_line_txt)
+    if match_result is None:
+      view.erase_regions(BULLET_LAST_POINT)
+    else:
+      last_line_start = sublime.Region(last_line.begin())
+      view.add_regions(BULLET_LAST_POINT, [last_line_start], BULLET_SCOPE, "circle")
 
   def last_pos(self, view):
     last_regions = view.get_regions(BULLET_LAST_POINT)
@@ -73,52 +82,80 @@ class Bullet(sublime_plugin.EventListener):
 
   def on_modified(self, view):
     if self.file_type > 0 and self.Modifying == False:
-      self.Modifying = True
-      insert_point = view.sel()[0].begin()
-      row, col = view.rowcol(insert_point)
-      last_cmd, last_cmd_args, last_cmd_count = view.command_history(0)
-      non_undo = view.command_history(1)[0] == None
-
-      # new line
       last_row = self.last_row(view)
-      if last_row is not None and row > 0 and abs(row - last_row) == 1:
-        self.bullet_event(view, insert_point, last_row)
-      # elif non_undo and last_cmd == "join_lines":
-        # TODO: remove leading bullet from joined lines
-      self.update_last_pos(view)
-      self.Modifying = False
+      if last_row is not None:
+        self.Modifying = True
+        current_point = view.sel()[0].begin()
+        current_row = view.rowcol(current_point)[0]
+        last_cmd, last_cmd_args, last_cmd_count = view.command_history(0)
+        non_undo = view.command_history(1)[0] == None
 
-  def bullet_event(self, view, insert_point, ref_row):
-    ref_line = view.line(view.text_point(ref_row, 0))
-    ref_row_start = ref_line.begin()
+        # new line
+        if current_row > 0 and abs(current_row - last_row) == 1:
+          self.add_or_remove_bullet(view)
+        elif non_undo and last_cmd == "join_lines":
+          self.join_bullet_lines(view)
+        self.Modifying = False
+
+  def add_or_remove_bullet(self, view):
+    ref_row_start = self.last_pos(view)
+    ref_line = view.line(ref_row_start)
     ref_line_txt = view.substr(ref_line)
     if ref_line_txt != "":
-      bullet_chars = self.bullet_chars[self.file_type].split()
-      bullet_re = "|".join(map(re.escape, bullet_chars))
-      search_re = "^( *|\t*)(%s|([0-9]+)\.) (.*)" % bullet_re
-      match_pattern = re.search(search_re, ref_line_txt)
-      if match_pattern != None:
-        if match_pattern.group(4) in [" ",""]:
+      match_result = self.match_bullet_line(ref_line_txt)
+      pre_bullet, bullet, num_bullet, bullet_contents = match_result.groups()
+      if match_result != None:
+        if bullet_contents in [" ",""]:
           # remove empty bullet point upon newline
           reg_remove = view.find("\S.*", ref_row_start)
           edit = view.begin_edit()
           view.erase(edit,reg_remove)
           view.end_edit(edit)
         else:
-          if match_pattern.group(3) != None:
+          if num_bullet != None:
             # insert incremented number
-            last_number = int(match_pattern.group(3))
+            last_number = int(num_bullet)
             insertion = str(last_number+1) + ". "
           else:
             # insert bullet
-            insertion = match_pattern.group(2) + " "
+            insertion = bullet + " "
           edit = view.begin_edit()
           # duplicate ref_line init whitespace
+          insert_point = view.sel()[0].begin()
           line_start = view.line(insert_point).begin()
           init_whitespace = sublime.Region(line_start, insert_point)
-          replacement = match_pattern.group(1)+insertion
+          replacement = pre_bullet+insertion
           view.replace(edit, init_whitespace, replacement)
           final_pos = view.sel()[0].end()
           view.sel().clear()
           view.sel().add(sublime.Region(final_pos))
           view.end_edit(edit)
+
+  def join_bullet_lines(self, view):
+    line_start = self.last_pos(view)
+    line = view.line(line_start)
+    line_txt = view.substr(line)
+    line_end = line.end()
+    match_result = self.match_bullet_line(line_txt)
+    bullet = match_result.groups()[1]
+    if bullet.isdigit():
+      bullet_re = "\d "
+    else:
+      bullet_re = re.escape(bullet)+" "
+
+    first_bullet = view.find(bullet_re, line_start) #ignore
+    search_start = first_bullet.end()
+    found_bullets = []
+    while True:
+      b = view.find(bullet_re, search_start)
+      if b is not None and b.end() < line_end:
+        found_bullets.append(b)
+        search_start = b.end()
+      else:
+        break
+
+    edit = view.begin_edit()
+    for b in reversed(found_bullets):
+      view.erase(edit, b)
+    view.end_edit(edit)
+
